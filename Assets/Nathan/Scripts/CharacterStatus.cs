76624d4ae2a7cb5db0e9e488a614357f3d4ff832
NathanN3189 <nilsen.rotuma.nathan9@gmail.com>
2022-07-09 14:22:54 +1000

    Nathan Content

---
+++ Assets/Nathan/Scripts/CharacterStatus.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+public class CharacterStatus : MonoBehaviour
+{
+    public int health;
+    public int level;
+    public int maxHealth;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
