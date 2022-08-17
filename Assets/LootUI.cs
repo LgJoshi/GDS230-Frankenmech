2fd8aac64a69102d5a4e0927458001e7bb0be364
NathanN3189 <nilsen.rotuma.nathan9@gmail.com>
2022-08-17 19:38:30 +1000

    Loot system update

---
+++ Assets/LootUI.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LootUI : MonoBehaviour
+{
+    SingletonDataStorage singletonDataStorage;
+    BattleSystem battleSystem;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        singletonDataStorage = GameObject.FindObjectOfType<SingletonDataStorage>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Return()
+    {
+        SceneManager.LoadScene("RoomGeneration");
+        battleSystem.LootMenuUI.SetActive(false);
+    }
+
+    public void Take()
+    {
+        singletonDataStorage.playerLimbLoadoutIds[0] = 4;
+
+        SceneManager.LoadScene("RoomGeneration");
+        battleSystem.LootMenuUI.SetActive(false);
+
+    }
+
+
+}
