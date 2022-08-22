1cfbae24322b9e01f654b69bdb6fa096f0f14f38
LgJoshi <80075442+LgJoshi@users.noreply.github.com>
2022-08-22 15:12:15 +1000

    Card grabber update
    Added sounds & line renderer

--- Assets/Scripts/Combat/GrabberOld.cs
+++ Assets/Scripts/Combat/GrabberOld.cs
@@ -2,35 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grabber : MonoBehaviour
+public class GrabberOld : MonoBehaviour
 {
     public GameObject grabbedObject;
     
     Vector3 grabbedObjectOrigin;
     int layer_mask;
-    LineRenderer lineRenderer;
-
-    AudioSource myAudio;
-    [SerializeField] AudioClip[] audioClips;
-
-    Vector3 camOffset = new Vector3(0, 8.08f, -10.58f);
     
     void Start()
     { 
         layer_mask = LayerMask.GetMask("Card");
         Debug.Log("layer_mask: " + layer_mask);
-
-        lineRenderer = this.GetComponent<LineRenderer>();
-        lineRenderer.positionCount = 2;
-        lineRenderer.enabled = false;
-
-        myAudio = this.GetComponent<AudioSource>();
     }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0)){
-            OnMouseDown();
+            if (grabbedObject==null){
+                RaycastHit hit = MouseCastRay();
+
+                if (hit.collider!=null){
+                    if (!hit.collider.CompareTag("Card")){
+                        return;
+                    }
+                    
+                    grabbedObject = hit.collider.gameObject;
+                    grabbedObjectOrigin = grabbedObject.transform.position;
+
+                    layer_mask = LayerMask.GetMask("CardInteractable");
+                }
+            }
         }
         
         if (Input.GetMouseButtonUp(0)){
@@ -43,7 +44,7 @@ public class Grabber : MonoBehaviour
         if (grabbedObject!=null){
             Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.WorldToScreenPoint(grabbedObject.transform.position).z);
             Vector3 worldPosition = Camera.main.ScreenToWorldPoint(position);
-            lineRenderer.SetPosition(1, new Vector3(worldPosition.x, worldPosition.y, worldPosition.z));
+            grabbedObject.transform.position=new Vector3(worldPosition.x, worldPosition.y, worldPosition.z);
         }
     }
 
@@ -74,31 +75,6 @@ public class Grabber : MonoBehaviour
         
     }
 
-    private void OnMouseDown()
-    {
-        if( grabbedObject == null )
-        {
-            RaycastHit hit = MouseCastRay();
-
-            if( hit.collider != null )
-            {
-                if( !hit.collider.CompareTag("Card") )
-                {
-                    return;
-                }
-
-                grabbedObject = hit.collider.gameObject;
-                grabbedObjectOrigin = grabbedObject.transform.position;
-
-                layer_mask = LayerMask.GetMask("CardInteractable");
-
-                lineRenderer.enabled = true;
-
-                lineRenderer.SetPosition(0, grabbedObject.transform.position);
-            }
-        }
-    }
-
     void OnMouseUp(){
         if (grabbedObject!=null){
 
@@ -114,23 +90,20 @@ public class Grabber : MonoBehaviour
                     objCardBehaviour.myEffect, 
                     objCardBehaviour.myEffectInt, 
                     objCardBehaviour.myEnergyCost
-                    ) == true) {
-
+                    ) == true)
+                {
                     //this event should delete the card
                     EventManager.CardPlayedFunction(objCardBehaviour.myHandId);
-
-                    myAudio.clip = audioClips[0];
-                    myAudio.Play();
+                } else
+                {
+                    //returns card to original position
+                    grabbedObject.transform.position = grabbedObjectOrigin;
                 }
-
-                lineRenderer.enabled = false;
 
             } else
             {
-                //if card fail
-                lineRenderer.enabled = false;
-                myAudio.clip = audioClips[1];
-                myAudio.Play();
+                //returns card to original position
+                grabbedObject.transform.position = grabbedObjectOrigin;
             }
 
             layer_mask = LayerMask.GetMask("Card");
